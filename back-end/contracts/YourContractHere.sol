fe23b2416dac90d6d5526c047b824f5833936eb2
robbie <robbiekruszynski@gmail.com>
2021-03-26 09:13:59 -0700

    all the notes

---
+++ back-end/contracts/YourContractHere.sol
@@ -0,0 +1,16 @@
+// If you’ve never heard of smart contacts, a way to think about them is; pieces of code that run on the blockchain (this can be local, test, or mainnet) that are guaranteed to produce the same results for everyone running them.
+
+// example
+// pragma solidity >=0.5.8;
+
+// contract SimpleStorage {
+//     uint256 storedData;
+
+//     function set(uint256 x) public {
+//         storedData = x;
+//     }
+
+//     function get() public view returns (uint256) {
+//         return storedData;
+//     }
+// }
---
+++ back-end/migrations/2_update_this.js
@@ -0,0 +1,10 @@
+// Migrations are JavaScript files that help you deploy contracts to the Ethereum network. These files are responsible for staging your deployment tasks, and they’re written under the assumption that your deployment needs will change over time. As your project evolves, you’ll create new migration scripts to further this evolution on the blockchain. 
+
+
+
+// example 
+// const SimpleStorage = artifacts.require("SimpleStorage.sol");
+
+// module.exports = function(deployer) {
+//  deployer.deploy(SimpleStorage);
+// };
